24ccbfdfb0d994aff6fc97cce32dc079b1af07c6
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-02 14:27:30 +0200

    Lagt til bedre mappestruktur. Og lagt inn kode for task1 og 2 for mandag uke1.

---
+++ Uke_1_Grunnleggende_og_Repetisjon/Mandag/Task1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1: Lag en ny C#-fil og skriv en klasse i et eget namespace.
+// 2: Skriv ut "Hei fra [Ditt navn]" i konsollen ved hjelp av Console.WriteLine();.
+
+namespace Task1;
+// Når man ikke bruker automatisk "Globad using" så må using System være i namespace
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hei fra Marcus!");
+    }
+}
---
+++ Uke_1_Grunnleggende_og_Repetisjon/Mandag/Task2/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2;
+
+// using System; -- Denne er kommentert ut. 
+
+//- Prøv å fjerne `using System;` fra toppen og skriv hele stien til `Console.WriteLine()` manuelt.
+// - Hva skjer, og hvorfor er dette upraktisk?
+//      - Må bruke "System.Console.Writeline", altså kalle på System hver gang jeg skal bruke systemfunksjoner. 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hilsen fra Marcus!");
+    }
+}
