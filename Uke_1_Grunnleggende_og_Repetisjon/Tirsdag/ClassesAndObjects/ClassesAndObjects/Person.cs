9dc5c45f21d9410ef3444f807efbd43ce7af8fdf
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-12 20:36:39 +0200

    Lagt til Person.cs som oppgaven sa.

---
+++ Uke_1_Grunnleggende_og_Repetisjon/Tirsdag/ClassesAndObjects/ClassesAndObjects/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassesAndObjects;
+
+/*
+Lag Person.cs og implementer:
+
+Feltene: Navn, Alder (helst som egenskaper)
+Metoden: PresenterDeg() – skriv ut en hyggelig presentasjon
+ */
+
+public class Person
+{
+    // begge propertiene er public for å kunen påvirkes utenfor klassen.
+    public string Name { get; set; } //Property for navn { get; set; } for å kunne sette og hente verdi. 
+    public int Age { get; set; } //Property for alder { get; set; } for å kunne sette og hente verdi.
+
+    public void PresenterDeg()
+    {
+        Console.WriteLine($"Hei, mitt navn er {Name} og jeg er {Age} år! Hyggelig å møte deg.");
+    }
+}
